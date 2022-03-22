7149e1eea3fc55b5e598c75d3b6568cab00e3697
Windawz <46968683+Windawz@users.noreply.github.com>
2022-03-22 12:00:14 +0300

    Expand the IPrecisionChecker interface family

---
+++ Solving/Solvers/IPrecisionSolver.TChecker.cs
@@ -0,0 +1,13 @@
+﻿using IterativeSolver.Solving.Magnifiables.PrecisionCheckers;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterativeSolver.Solving.Solvers;
+internal interface IPrecisionSolver<TChecker> : IPrecisionSolver 
+    where TChecker : IPrecisionChecker {
+    new TChecker PrecisionChecker { get; }
+}
---
+++ Solving/Solvers/IPrecisionSolver.cs
@@ -0,0 +1,12 @@
+﻿using IterativeSolver.Solving.Magnifiables.PrecisionCheckers;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterativeSolver.Solving.Solvers;
+internal interface IPrecisionSolver : ISolver {
+    IPrecisionChecker PrecisionChecker { get; }
+}
